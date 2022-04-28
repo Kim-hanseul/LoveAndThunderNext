9decdacd55ebdf9dccabfb8572d265562441c993
Kim-hanseul <hansul80@gmail.com>
2022-04-28 12:55:31 +0900

    2022.04.28 basic structure

--- components/Layout.js
+++ components/Layout.js
@@ -4,12 +4,9 @@ import {Footer, Header, Modal, Nav, Pagination, Table } from "@/components";
 export function Layout({ children }){
   return (
       <div className={styles.container}>
-        <Header/>
-        <Nav/>
         <main className={styles.main}>{children}</main>
         <Table/>
         <Pagination/>
-        <Footer/>
         <Modal/>
       </div>
   );
